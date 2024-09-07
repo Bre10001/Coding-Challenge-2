b2db9498650a30a2d2fee7f310b87771d528e1bf
Breanna Burris <breanna1burris@gmail.com>
2024-09-07 17:54:53 -0400

    Initial logic for calculationg tip using ternaryOp

---
+++ cc_2.js
@@ -0,0 +1,28 @@
+// Task 1 Calculate the Tip
+
+// Declaration of bill
+let bill = 100;
+
+// Calculation of tip based on bill size using if-else statements
+
+if (bill > 300) {
+    let tip = 0.20;
+    tip = bill * tip;
+    console.log(`The tip amount is: $${tip}`) ; //Displays 20% tip if greater than 300
+}
+
+else if (bill >= 50 || bill <= 300) {
+    let tip = 0.15;
+    tip = bill * tip;
+    console.log(`The tip amount is: $${tip}`); //Displays 15% tip if bill is between $50 and $300
+}
+
+else {
+    console.log(`Tips are appreciated!`);
+}
+
+
+
+
+
+
